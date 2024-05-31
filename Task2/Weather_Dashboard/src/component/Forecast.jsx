076617140da4e55eaef964284d18939a4aacbaae
armanahad6207 <armanahad7549@gmail.com>
2024-05-31 23:35:53 +0530

    create onecallapi.formate forcastdata

--- Task2/Weather_Dashboard/src/component/Forecast.jsx
+++ Task2/Weather_Dashboard/src/component/Forecast.jsx
@@ -6,30 +6,42 @@ function forecast({ title, keyprop }) {
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-col items-center p-4 leading-8">
             <span>04:03 PM</span>
-            <span className="material-icons  text-red-500 leading-8">
-              sunny
-            </span>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              className="w-12"
+            />
             <span className="text-xl font-medium leading-8">22°</span>
           </div>
           <div className="flex flex-col items-center p-4 leading-8">
             <span>04:03 PM</span>
-            <span className="material-icons  text-red-500 leading-8">
-              sunny
-            </span>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              className="w-12"
+            />
             <span className="text-xl font-medium leading-8">22°</span>
           </div>
           <div className="flex flex-col items-center p-4 leading-8">
             <span>04:03 PM</span>
-            <span className="material-icons  text-red-500 leading-8">
-              sunny
-            </span>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              className="w-12"
+            />
             <span className="text-xl font-medium leading-8">22°</span>
           </div>
           <div className="flex flex-col items-center p-4 leading-8">
             <span>04:03 PM</span>
-            <span className="material-icons  text-red-500 leading-8">
-              sunny
-            </span>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              className="w-12"
+            />
+            <span className="text-xl font-medium leading-8">22°</span>
+          </div>
+          <div className="flex flex-col items-center p-4 leading-8">
+            <span>04:03 PM</span>
+            <img
+              src="http://openweathermap.org/img/wn/01d@2x.png"
+              className="w-12"
+            />
             <span className="text-xl font-medium leading-8">22°</span>
           </div>
         </div>
--- Task2/Weather_Dashboard/src/services/WeatherApi.js
+++ Task2/Weather_Dashboard/src/services/WeatherApi.js
@@ -1,8 +1,10 @@
+import { DateTime } from "luxon";
 const api_key = "3ce7301240d18d5b5c6671fca3722abb";
 const base_url = "https://api.openweathermap.org/data/2.5";
 
 // "https://api.openweathermap.org/data/2.5/weather?q=ranchi&appid=3ce7301240d18d5b5c6671fca3722abb"
 
+// custom function for api calling
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(base_url + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: api_key });
@@ -11,6 +13,7 @@ const getWeatherData = (infoType, searchParams) => {
   return apiData;
 };
 
+// formate the data from api
 const formatedData = (data) => {
   const {
     coord: { lat, lon },
@@ -41,11 +44,55 @@ const formatedData = (data) => {
   };
 };
 
+// format Forcast data
+const formatForcastData = (data) => {
+  let { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map((d) => {
+    return {
+      title: formatLocalTime(d.dt, timezone, "ccc"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
+  hourly = hourly.slice(1, 6).map((d) => {
+    return {
+      title: formatLocalTime(d.dt, timezone, "hh:mm a"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
+  return { timezone, daily, hourly };
+};
+
 const getFormatedWeatherData = async (searchParams) => {
   const formatCurrentWeather = await getWeatherData(
     "weather",
     searchParams
   ).then(formatedData);
-  console.log(formatCurrentWeather);
+
+  // onecallapi
+  const { lat, lon } = formatCurrentWeather;
+
+  const formatForcastWeather = await getWeatherData("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: searchParams.units,
+  }).then(formatForcastData);
+  console.log(" formate weather" + formatCurrentWeather);
+  console.log(" formate forecast" + formatForcastWeather);
+
+  return { ...formatCurrentWeather, ...formatForcastWeather };
 };
+
+const formatLocalTime = (
+  secs,
+  zone,
+  format = "cccc,dd LL yyyy', | Local time: 'hh:mm:a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
+const iconUrlfromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
 export default getFormatedWeatherData;
+
+export { formatLocalTime, iconUrlfromCode };
